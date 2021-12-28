3d0dacc8bc5d82cc514ab26e559e8fe6fff6a5b7
Stephan Strittmatter <stephan@st-strittmatter.name>
2021-12-28 15:23:55 +0100

    update favicons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,17 @@ module.exports = {
   title: 'Feinstaub in Stockach',
   description: '.',
   repo: 'https://github.com/stritti/stockach-feinstaub-website',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#3a0839"}],
+    ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+    ['meta', { name: "msapplication-config", content: "/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   theme: path.resolve(__dirname, './theme'),
 
   themeConfig: {
